52411ae6b44838fcdff996e1264e9c59d63c8365
Mukul Mangde <102212321+MukulMangde@users.noreply.github.com>
2022-04-22 22:27:12 +0530

    Add files via upload

---
+++ 3_Implementation/main.c
@@ -0,0 +1,19 @@
+#include <avr/io.h>
+#include<util/delay.h>
+
+int main(void)
+{
+    DDRB=DDRB&0b11111101;
+    DDRC=DDRC|0b01000000;
+
+    while(1)
+        {
+        if(PINB & 0b00000010)
+        PORTC=PORTC&0b10111111;
+
+        else
+        PORTC=PORTC|0b01000000;
+
+    }
+    return 0;
+}
